0dcd4f9fa13f63f30417589f45bb36209b029e66
1recyclebin1 <96552851+1recyclebin1@users.noreply.github.com>
2026-01-07 17:41:13 +0100

    Added moreflags for Metyr's Quest skip

--- TarnishedTool/GameIds/Event.cs
+++ TarnishedTool/GameIds/Event.cs
@@ -11,7 +11,8 @@ public static class Event
     public static readonly long SeeUndergroundGraces = 82001;
     public static readonly long SeeDlcGraces = 82002;
 
-    public static readonly long[] UnlockMetyr = [2051450180, 9440]; //Invader + quest
+    public static readonly long[] UnlockMetyr = [2050400600,2053460600,2051459226,2051459228,2051459229,2051459230,2051455023,2051459249,2051452717,
+                                                 2050407000,400662,4856,4855,4854,4849,2051452718,2051459213,2051450715,9440,2051450180]; //Progress Ymir's Quest, get rid of the Invader, move chair and enable fight with metyr
 
     public static readonly long FightFortissax = 12032859;
     public static readonly long FightEldenBeast = 19002802;
