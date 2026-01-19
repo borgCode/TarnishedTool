1615d08a99df47d113c36ca127f9c3008f2a61f8
1recyclebin1 <baselayed99@yahoo.com>
2026-01-19 01:31:19 +0100

    Commenting out broken Weather Types and fixing boss revive naming

--- TarnishedTool/Enums/WeatherType.cs
+++ TarnishedTool/Enums/WeatherType.cs
@@ -2,23 +2,25 @@ namespace TarnishedTool.Enums;
 
 public enum WeatherType
 {
+    Default = 0,
+    Rainy = 1,
+    Snowy = 2,
+    WindyRain = 3,
+    Foggy  = 4,
+    FlatClouds = 6,
+    WindyPuffyClouds = 12,
+    RainyHeavyFog = 15,
+    ScatteredRain = 17,
+    /*
+    --Currently Broken-- 
     NoWeather = -1,
-    DefaultWeather = 0,
-    RainWeather = 1,
-    SnowWeather = 2,
-    WindyRainWeather = 3,
-    FogWeather = 4,
     ClearWeather = 5,
-    FlatCloudsWeather = 6,
     PuffyCloudsWeather = 7,
     RainyCloudsWeather = 8,
     WindyFogWeather = 9,
     HeavySnowWeather = 10,
     HeavyFogWeather = 11,
-    WindyPuffyCloudsWeather = 12,
-    RainyHeavyFogWeather = 15,
     SnowyHeavyFogWeather = 16,
-    ScatteredRainWeather = 17,
     UnkWeather = 18
-        
+     */
 }
