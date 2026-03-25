ee37f93b9729b90acc2cf8d21964f3ed208aab57
1recyclebin1 <baselayed99@yahoo.com>
2026-03-25 10:08:26 +0100

    fixing mute music

--- TarnishedTool/Services/SettingsService.cs
+++ TarnishedTool/Services/SettingsService.cs
@@ -1,6 +1,8 @@
 ﻿// 
 
+using System;
 using TarnishedTool.Interfaces;
+using TarnishedTool.Utilities;
 using static TarnishedTool.Memory.Offsets;
 
 namespace TarnishedTool.Services;
@@ -11,7 +13,8 @@ public class SettingsService(IMemoryService memoryService) : ISettingsService
         memoryService.Write(memoryService.Read<nint>(GameMan.Base) + GameMan.ShouldQuitout, (byte)1);
 
     public void ToggleStutterFix(bool isEnabled) =>
-        memoryService.Write(memoryService.Read<nint>(UserInputManager.Base) + UserInputManager.SteamInputEnum, isEnabled);
+        memoryService.Write(memoryService.Read<nint>(UserInputManager.Base) + UserInputManager.SteamInputEnum,
+            isEnabled);
 
     public void ToggleDisableAchievements(bool isEnabled)
     {
@@ -25,10 +28,33 @@ public class SettingsService(IMemoryService memoryService) : ISettingsService
     public void ToggleNoLogo(bool isEnabled) =>
         memoryService.WriteBytes(Patches.NoLogo, isEnabled ? [0x90, 0x90] : [0x74, 0x53]);
 
+    private byte? _originalMusicVolume = SettingsManager.Default.HasOriginalMusicVolume
+        ? SettingsManager.Default.OriginalMusicVolume
+        : null;
+
     public void ToggleMuteMusic(bool isMuteMusicEnabled)
     {
         var optionsPtr =
             memoryService.Read<nint>(memoryService.Read<nint>(GameDataMan.Base) + GameDataMan.Options);
-        memoryService.Write(optionsPtr + (int)GameDataMan.OptionsOffsets.Music, (byte)0);
+        var musicPtr = optionsPtr + (int)GameDataMan.OptionsOffsets.Music;
+
+        if (isMuteMusicEnabled)
+        {
+            if (!SettingsManager.Default.HasOriginalMusicVolume)
+            {
+                _originalMusicVolume = memoryService.Read<byte>(musicPtr);
+                SettingsManager.Default.OriginalMusicVolume = _originalMusicVolume.Value;
+                SettingsManager.Default.HasOriginalMusicVolume = true;
+                SettingsManager.Default.Save();
+            }
+
+            memoryService.Write(musicPtr, (byte)0);
+        }
+        else
+        {
+            memoryService.Write(musicPtr, SettingsManager.Default.OriginalMusicVolume);
+            SettingsManager.Default.HasOriginalMusicVolume = false;
+            SettingsManager.Default.Save();
+        }
     }
 }
