887a0d4b2ba4b48fd42c235cebab04e068368353
Recycle <96552851+1recyclebin1@users.noreply.github.com>
2026-05-04 13:36:13 +0200

    torrent anywhere fix for abyssal woods (#97)

--- TarnishedTool/GameIds/SpEffect.cs
+++ TarnishedTool/GameIds/SpEffect.cs
@@ -5,6 +5,7 @@ namespace TarnishedTool.GameIds;
 public static class SpEffect
 {
     public const uint RuneArc = 3450;
+    public const uint ForcedDismount = 19995;
     public const uint SleepArrow = 6454;
     public const uint SleepPot = 500640;
 }
