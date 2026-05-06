a778d5cb3086bc845e1e2ce1a7de0a25c659d3f1
Recycle <96552851+1recyclebin1@users.noreply.github.com>
2026-05-06 07:35:18 +0200

    Spirit Tuning button (#101)

--- TarnishedTool/GameIds/EzState.cs
+++ TarnishedTool/GameIds/EzState.cs
@@ -68,6 +68,7 @@ public static class EzState
         public static readonly TalkCommand Rebirth = new(113, []);
         public static readonly TalkCommand OpenPhysick = new(130, []);
         public static readonly TalkCommand OpenMirror = new(81, []);
+        public static readonly TalkCommand OpenSpiritTuning = new(136, []);
 
         public static TalkCommand AcquireGesture(int gestureId) => new(131, [gestureId]);
 
