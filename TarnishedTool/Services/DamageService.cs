42a411a35698ccaaf51a85e17b393f81672eccd6
perunavich <125222551+perunavich@users.noreply.github.com>
2026-07-07 00:17:35 +0200

    Team-aware damage multiplier

--- TarnishedTool/Services/DamageService.cs
+++ TarnishedTool/Services/DamageService.cs
@@ -29,12 +29,12 @@ public class DamageService(IMemoryService memoryService, HookManager hookManager
             memoryService.Write(inFactor, incoming);
 
             var bytes = AsmLoader.GetAsmBytes(AsmScript.DamageMultiplier);
-            AsmHelper.WriteAbsoluteAddresses(bytes, [(WorldChrMan.Base.ToInt64(), 0x20 + 2)]);
-            AsmHelper.WriteImmediateDwords(bytes, [(WorldChrMan.PlayerIns, 0x36 + 3)]);
+            AsmHelper.WriteAbsoluteAddresses(bytes, [(WorldChrMan.Base.ToInt64(), 0x2C + 2)]);
+            AsmHelper.WriteImmediateDwords(bytes, [(WorldChrMan.PlayerIns, 0x3E + 3)]);
             AsmHelper.WriteRelativeOffsets(bytes, [
-                (code.ToInt64() + 0x49, outFactor.ToInt64(), 8, 0x49 + 4),
-                (code.ToInt64() + 0x53, inFactor.ToInt64(), 8, 0x53 + 4),
-                (code.ToInt64() + 0x8C, hookLoc + 0x7, 5, 0x8C + 1)
+                (code.ToInt64() + 0x72, outFactor.ToInt64(), 8, 0x72 + 4),
+                (code.ToInt64() + 0x7C, inFactor.ToInt64(), 8, 0x7C + 4),
+                (code.ToInt64() + 0xB5, hookLoc + 0x7, 5, 0xB5 + 1)
             ]);
             memoryService.WriteBytes(code, bytes);
             hookManager.InstallHook(code.ToInt64(), hookLoc, [0x41, 0x8B, 0x96, 0x28, 0x02, 0x00, 0x00]);
